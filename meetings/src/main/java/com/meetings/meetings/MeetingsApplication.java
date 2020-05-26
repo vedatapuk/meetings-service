946c2cb001d884f7987e59411bbd81b59b4dd6eb
Rinori99 <rinori99@live.com>
2020-05-26 12:22:09 +0200

    gave application a discoverable name and connected to service discovery

--- meetings/src/main/java/com/meetings/meetings/MeetingsApplication.java
+++ meetings/src/main/java/com/meetings/meetings/MeetingsApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MeetingsApplication extends SpringBootServletInitializer {
 
